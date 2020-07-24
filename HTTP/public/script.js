6501ada66233941316bfc346c03b113d223ef9b6
Natan Tavares <nattantavares.s15@gmail.com>
2020-07-24 19:43:07 -0300

    Refactory template

--- HTTP/public/script.js
+++ HTTP/public/script.js
@@ -1,20 +1,30 @@
 console.log('Script working');
 async function loadUrls() {
   const result = await fetch("http://localhost:3000/").then(data => data.json());
-  console.log('> result.urls:', result.urls);
-
   result.urls.map(({ name, url }) => addUrl({ name, url }));
 }
 
 function addUrl({ name, url }) {
-  const body = document.querySelector('body');
-  const a = document.createElement('a');
+  const ul = document.querySelector('.list-url');
+  const a = document.createElement('a')
+  const card = document.createElement('li');
 
-  body.appendChild(a);
+  card.classList.add('card');
+  card.innerHTML = `
+    <h3>name: ${name}</h3>
+    <h4>url: <a href="${url}" target="_black">${url}</a></h4>
+  `;
 
-  a.innerHTML = name;
+  ul.appendChild(card);
+
+  a.innerHTML = url;
   a.target = '_black';
   a.href = url;
+
+  // <li class="card">
+  //   <h3>name: Contato</h3>
+  //   <h4>url: <a href="https://linktr.ee/NatanTavares" target="_black">https://linktr.ee/NatanTavares</a></h4>
+  // </li>
 }
 
 loadUrls();
